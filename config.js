5c7dd9e4258e960cfbc19d5ac0fc1c10542c5f14
titi49600 <44643076+titi49600@users.noreply.github.com>
2023-06-02 14:05:57 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+{
+  "token": "TOKEN",
+  "channelId": "CHANELID",
+  "imageDirectory": "IMAGE File",
+  "uploadDelay": 3000,
+  "largeFilesDirectory": " ",
+  "allowedFileTypes": [".mov", ".mp4", ".gif.mp4"],
+  "uploadedFilesDirectory": " "
+}
